ed37daa6bcaaaeb8704342e010db115ff55af8e8
Michał <michal.gawryluk@icloud.com>
2018-12-09 11:24:24 +0100

    Initial Commit

---
+++ PhotoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoApp
+//
+//  Created by Michał on 09/12/2018.
+//  Copyright © 2018 Michał Gawryluk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
